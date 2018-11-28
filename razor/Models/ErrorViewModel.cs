f5f1194dad86e831c887c7055ccb74a856fca90b
prescottbreeden <prescott.breeden@gmail.com>
2018-11-28 00:16:30 -0800

    can I go back to python now?

---
+++ razor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace razor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
