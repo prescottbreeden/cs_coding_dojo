bb009d3f11d22bb6b5af7bc70488863918a9b90e
Prescott Breeden <prescott.breeden@gmail.com>
2018-12-02 20:19:06 -0800

    started adding a form to quoting dojo, added mac connection string to db connection

--- quoting_dojo/Config/DbConnection.cs
+++ quoting_dojo/Config/DbConnection.cs
@@ -10,11 +10,13 @@ namespace quoting_dojo.DbConfig
     static string db = "quoting_dojo_cs";
     static string port = "3306";
     static string user = "trashpanda";
+    static string macUser = "root";
     static string pw = "rubberbabybuggybumpers";
+    static string macPw = "root";
     internal static IDbConnection DatabaseConnection 
     {
       get {
-        return new MySqlConnection($"Server={server};Port={port};Database={db};UserID={user};Password={pw};SslMode=None");
+        return new MySqlConnection($"Server={server};Port={port};Database={db};UserID={macUser};Password={macPw};SslMode=None");
       }
     }
   }
