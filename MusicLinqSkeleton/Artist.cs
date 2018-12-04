2dc49a81198f7ba7f5538cfa73ba5130567d2a6d
prescottbreeden <prescott.breeden@gmail.com>
2018-12-03 17:47:30 -0800

    linq stuff

---
+++ MusicLinqSkeleton/Artist.cs
@@ -0,0 +1,18 @@
+namespace JsonData 
+{
+    public class Artist 
+    {
+        public string ArtistName;
+        public string RealName;
+        public int Age;
+        public string Hometown;
+        public int GroupId;
+        public Group Group;
+
+        public override string ToString()
+        {
+            return $"{ArtistName} is from {Hometown}";
+        }
+    }
+
+}
---
+++ MusicLinqSkeleton/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JsonData;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Collections to work with
+            List<Artist> Artists = JsonToFile<Artist>.ReadJson();
+            List<Group> Groups = JsonToFile<Group>.ReadJson();
+
+            //========================================================
+            //Solve all of the prompts below using various LINQ queries
+            //========================================================
+
+            //There is only one artist in this collection from Mount Vernon, what is their name and age?
+            var mountVernon = Artists.First(a => a.Hometown == "Mount Vernon");
+            System.Console.WriteLine(mountVernon);
+
+            //Who is the youngest artist in our collection of artists?
+            var youngest = Artists.Min(a => a.Age);
+            var artist = Artists.Where(a => a.Age == youngest);
+            System.Console.WriteLine(artist.FirstOrDefault().ArtistName + " is " + youngest + " years old.");
+
+            //Display all artists with 'William' somewhere in their real name
+
+            //Display the 3 oldest artist from Atlanta
+
+            //(Optional) Display the Group Name of all groups that have members that are not from New York City
+
+            //(Optional) Display the artist names of all members of the group 'Wu-Tang Clan'
+	    Console.WriteLine(Groups.Count);
+        }
+    }
+}
